bd592497d1eb278dbd322db39de01420015c2945
SergioMunozRoca <sermunroc@alu.edu.gva.es>
2025-01-24 14:58:51 +0100

    Create Forma.java

---
+++ Formas.docs/Forma.java
@@ -0,0 +1,18 @@
+package Tarea3_Actividad4;
+
+public class Forma {
+    protected String nombre;
+
+    public Forma(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
