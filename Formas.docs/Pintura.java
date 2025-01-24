bf3e72a980003d8322bb80eb65bed18c939c092a
SergioMunozRoca <sermunroc@alu.edu.gva.es>
2025-01-24 15:00:11 +0100

    Create Pintura.java

---
+++ Formas.docs/Pintura.java
@@ -0,0 +1,13 @@
+package Tarea3_Actividad4;
+
+public class Pintura {
+    protected double cobertura;
+
+    public Pintura(double cobertura) {
+        this.cobertura = cobertura;
+    }
+
+    public double cantidadPint(Forma forma){
+        return forma.area()/cobertura;
+    }
+}
