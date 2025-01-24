199d4a1d2a3961ac19dfc2235a2d4be7b5a9d168
SergioMunozRoca <sermunroc@alu.edu.gva.es>
2025-01-24 14:59:34 +0100

    Create Rectangulo.java

---
+++ Formas.docs/Rectangulo.java
@@ -0,0 +1,17 @@
+package Tarea3_Actividad4;
+
+public class Rectangulo extends Forma{
+    protected double longitud;
+    protected double ancho;
+
+    public Rectangulo(String nombre, double longitud, double ancho) {
+        super(nombre);
+        this.longitud = longitud;
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double area(){
+        return ancho * longitud;
+    }
+}
