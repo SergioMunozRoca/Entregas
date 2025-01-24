952a2f693350a7153da49c4f0ca5a5c908eac408
SergioMunozRoca <sermunroc@alu.edu.gva.es>
2025-01-24 08:51:56 +0100

    Update impresora.java

--- impresora.docs/impresora.java
+++ impresora.docs/impresora.java
@@ -1,1 +1,36 @@
+package IMPRESORA;
 
+public class Impresora {
+    private int nivelToner;
+    private int pagImpresas;
+    private boolean dobleCara;
+
+    public Impresora(int nivelToner, int pagImpresas, boolean dobleCara) {
+        this.nivelToner = nivelToner;
+        this.pagImpresas = pagImpresas;
+        this.dobleCara = dobleCara;
+    }
+
+    public int agregarToner(int cantidadToner){
+        nivelToner = nivelToner + cantidadToner;
+
+        if(nivelToner < 0 || nivelToner > 100){
+            return -1;
+        }
+        System.out.println("La cantidad total de tóner es: " + nivelToner);
+        return cantidadToner;
+    }
+
+    public int imprimirPaginas(int pagAimprimir){
+
+        if(dobleCara){
+            pagAimprimir = pagAimprimir * 2;
+        }
+        pagImpresas = pagImpresas + pagAimprimir;
+        return pagAimprimir;
+    }
+
+    public int getPaginasImpresas() {
+        return pagImpresas;
+    }
+}
