e87edb1b25d98e13722ab44037bae78ada8713f0
SergioMunozRoca <sermunroc@alu.edu.gva.es>
2025-02-08 11:42:15 +0100

    Create Colores.java

---
+++ Telefono.docs/Colores.java
@@ -0,0 +1,7 @@
+package Ejercicio1;
+
+public class Colores {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String CYAN = "\u001B[36m";
+}
